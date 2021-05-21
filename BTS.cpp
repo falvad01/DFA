5230bf6136b7ab7ae79d355c403e540fe377d8a8
falvarez <01157630@pw.edu.pl>
2021-05-21 19:16:50 +0200

    First

---
+++ BTS.cpp
@@ -0,0 +1,5 @@
+//
+// Created by javi on 21/5/21.
+//
+
+#include "BTS.h"
---
+++ BTS.h
@@ -0,0 +1,13 @@
+//
+// Created by javi on 21/5/21.
+//
+
+#ifndef UNTITLED_BTS_H
+#define UNTITLED_BTS_H
+
+class BTS {
+
+};
+
+
+#endif //UNTITLED_BTS_H
